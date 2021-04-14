6e0e614d3facd7ec000d7b9a1e9ad2680686abb8
Harsh-Vashistha <45832558+Harsh-Vashistha@users.noreply.github.com>
2021-04-14 15:32:43 +0530

    events added

--- contracts/Chain.sol
+++ contracts/Chain.sol
@@ -5,55 +5,101 @@ contract Chain{
     uint public productCount = 0;
 
 
+/*  
+    id - It will be used to store product in map and to track the product 
+    productName - stores name of the item
+    owner - It is of type address that represent account of owner that uniqely identifies the user
+    location - stores the location of the item at a particular stage of supply chain
+*/
+
     struct Item{
-        
-        uint id;  /*id will be used to store product in map and to track the product */
+        uint id;
+        uint ownerCount;
         string productName;
-        address owner; //will be changed from string to address
-        string location;  /* store location of a product at a cetrain time */
     }
-    mapping(uint => Item) public itemArray;
+
+    struct OwnerDetails{
+        address owner;
+        string location;
+    }
+/* itemArray stores history of a product */
+    mapping(uint => Item) public itemDetails;
+    mapping(uint => OwnerDetails[]) public itemOwnerHistory;
+
+    mapping (uint => address) public itemCurrentOwner;
+
+    event addProductCompleted(
+        uint _id,
+        string _productName,
+        string _location
+    );
+
+    event transferOwnerCompleted(
+        uint _id,
+        string _location
+    );
 
     constructor() public{
         //owner =msg.sender;
-        storeProduct(0,"first product",0xDEE7796E89C82C36BAdd1375076f39D69FafE252,'delhi');
+        addProduct(0,"first product",'delhi');
     }
 
-   /* replaced by the function storeProduct
-    function setInfo(uint _id,string memory _productName,address _owner,string memory _location) public{
-        productCount++;
-        itemArray[_id]=Item(_id, _productName,_owner,_location);
-        //itemArray[productCount]=Item(_id,_productName,_location);
-    }*/
-
-
-
-    /* check whether a product with tracking id already exists in 
-    array then return true else return true */
+/*
+    check whether a product with tracking id already exists in 
+    array then return true else return true 
+*/
 
     function productExists(uint _id) view public returns(bool){
-        return (itemArray[productCount].owner!=address(0)?true:false);    //replace productCount with _id productCount is for development
+        return (itemCurrentOwner[_id]!=address(0)?true:false);    //replace productCount with _id productCount is for development
     }
 
-    /*add new item in the array with the tracking id _id if item  already exist do not add second time*/
-    function storeProduct(uint _id,string memory _productName,address _owner,string memory _location) public returns(bool) {
+
+/*
+     let manufacturer to add new item in the list if item already exists then return without adding
+     item have a unique id _id , function update current owner of the product in itemOwner mapping
+     and push new owner data in itemArray mapping (contains all history of the product)
+*/
+
+//add address _owner arg 
+    function addProduct(uint _id,string memory _productName,string memory _location) public returns(bool) {
 
         if(productExists(_id)){
-          
-            return false;
+            
+            return false; // item already exists
         }
+        itemCurrentOwner[_id]=msg.sender;
         productCount++;
-        itemArray[productCount]=Item(_id,_productName,_owner,_location); //replace productCount with _id productCount is for development
+        itemDetails[_id]=Item(_id,0,_productName);
+        
+        itemDetails[_id].ownerCount++; // increase the count of owners of a product
+
+        OwnerDetails memory a=OwnerDetails(msg.sender,_location);
+        itemOwnerHistory[_id].push(a);
+
+        emit addProductCompleted(_id, _productName, _location);
+
     }
 
-
-    /* scan item to store whereabouts of the product*/
-    function scanShipment() public {
-        uint _id; //id of the shipment scanned usign QR code
-        if(msg.sender==itemOwner[_id]){
+    
+    /*
+    scan item to store whereabouts of the product and change the owner of the product
+    */
+    function transferOwner(uint _id,string memory _location) public returns(bool){
+        //id of the shipment scanned usign QR code
+        if(msg.sender==itemCurrentOwner[_id]){
             //item is already owner of the item
             return false;
         }
-    }    
-    /* function that allow a client to remove any item */
+
+        itemCurrentOwner[_id]=msg.sender;
+
+        OwnerDetails memory a=OwnerDetails(msg.sender,_location);
+        itemOwnerHistory[_id].push(a);
+
+        emit transferOwnerCompleted(_id, _location);
+    }
+    
+/*
+    function to return all whereabouts of the product with product-id _id
+ */
 }
