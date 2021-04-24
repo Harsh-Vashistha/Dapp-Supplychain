1a6e3ee6b275f2ca6d188c04b865afc4c7efd72f
Harsh-Vashistha <45832558+Harsh-Vashistha@users.noreply.github.com>
2021-04-24 22:54:57 +0530

    metamask enabled

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,23 @@
+const HDWalletProvider=require('@truffle/hdwallet-provider');
+const infuraKey="6741bd65e9ef41fbb8cc76b45b2d5350";
+const fs=require("fs");
+const memonic=fs.readFileSync('./.secret').toString().trim();
+
+
 module.exports={
   networks:{
-    develpoment:{
+    development:{
       host:"127.0.0.1",
-      port:7545,
+      port:8545,
       network_id:"*"//match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(
+        memonic, "wss://ropsten.infura.io/ws/v3/6741bd65e9ef41fbb8cc76b45b2d5350", 0
+        ),
+      network_id: 3,
+      gas: 4700000,
+      gasPrice: 100000000000
     }
   },
   solc:{
